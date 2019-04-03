294e548528a52b41903313c7dcd210b5648d7eef
noystl <noystll@gmail.com>
2019-04-03 21:24:18 +0300

    skeleton thread

---
+++ thread.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jonathan Weiss on 2019-04-03.
+//
+
+#include "thread.h"
---
+++ thread.h
@@ -0,0 +1,14 @@
+//
+// Created by Jonathan Weiss on 2019-04-03.
+//
+
+#ifndef EX2_THREAD_H
+#define EX2_THREAD_H
+
+
+class thread {
+
+};
+
+
+#endif //EX2_THREAD_H
